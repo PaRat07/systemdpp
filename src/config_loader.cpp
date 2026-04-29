b3fad66c3fea4c3c364c0d485b961c160478eddf
PaRat07 <pasha.at.phone@gmail.com>
2026-04-29 20:17:25 +0300

    basic config test

--- src/config_loader.cpp
+++ src/config_loader.cpp
@@ -6,10 +6,17 @@
 #include <fstream>
 
 Config LoadConfig(const std::filesystem::path& path) {
-  std::fstream config_file(path);
+  std::ifstream config_file(path);
+  if (!config_file.is_open()) {
+    throw std::runtime_error("Failed to open file: " + path.string());
+  }
   return rfl::toml::read<Config>(config_file)
     .or_else([] (rfl::Error&& err) -> std::expected<Config, rfl::Error> {
       throw std::runtime_error(std::move(err).what());
     })
     .value();
+}
+
+std::string Dump(Config conf) {
+  return rfl::toml::write(conf);
 }
