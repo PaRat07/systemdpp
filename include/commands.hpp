b93b9331e4e8c3609614f5006633637093daf202
PaRat07 <pasha.at.phone@gmail.com>
2026-05-06 13:27:20 +0300

    impl

--- include/commands.hpp
+++ include/commands.hpp
@@ -3,5 +3,19 @@
 #include <vector>
 #include <string>
 
+enum class ServiceStatus {
+  Inactive,
+  Active,
+  Failed,
+};
+
+struct ServiceState {
+  ServiceStatus state;
+  int exit_code;
+  int pid;
+};
+
 void Start(std::vector<std::string> services);
 void Stop(std::vector<std::string> services);
+ServiceState Status(const std::string& service);
+
